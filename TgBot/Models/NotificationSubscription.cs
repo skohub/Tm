a94a70c545a8958606e5f4bbdb339eafd7037ae9
skohub <skocraft@gmail.com>
2023-02-26 11:30:10 -0500

    fixed type

--- TgBot/Models/NotificationSubscription.cs
+++ TgBot/Models/NotificationSubscription.cs
@@ -5,5 +5,5 @@ public class NotificationSubscription
     public string UserPattern { get; set; } = string.Empty;
     // Template can contain @msg, @date, @user tags
     public string NotificationTemplate { get; set; } = string.Empty;
-    public int ChatId { get; set; }
+    public long ChatId { get; set; }
 }
